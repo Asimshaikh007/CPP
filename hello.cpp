2feca9ff2faadebe14052dc4106a47391a806c44
Asimshaikh007 <shaikhasim6165@gmail.com>
2025-02-22 17:22:42 +0530

    Hello

---
+++ hello.cpp
@@ -0,0 +1,7 @@
+#include<iostream>// iostream is a header file which is used to include the cout object of std namespace
+using namespace std;// using namespace std; is used to access the cout object of std namespace
+int main(){
+    cout<<"Hello World!"<<"\n";
+    std::cout<<"Welcome To C++ Programming"<<"\n"; // std::cout is used to access the cout object of std namespace
+    return 0;
+}
